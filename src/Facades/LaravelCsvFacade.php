add81468021aed0504328f13a97a9c905e71dc9b
muhammadislom <islom.pr@gmail.com>
2023-12-21 17:28:41 +0500

    psr-4

--- src/Facades/LaravelCsvFacade.php
+++ src/Facades/LaravelCsvFacade.php
@@ -5,5 +5,8 @@ use Illuminate\Support\Facades\Facade;
 
 class LaravelCsvFacade extends Facade
 {
-
+    protected static function getFacadeAccessor()
+    {
+        return 'openkeycode.laravel-scv';
+    }
 }
